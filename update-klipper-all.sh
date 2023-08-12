37b8ec9bd9cbb7e7475b936e72cf79ac19f4d79b
Conor Duffy <static@staticanime.net>
2023-08-12 19:44:55 +0100

    Scripted auto-commit on change (2023-08-12 19:44:55) by gitwatch.sh

---
+++ update-klipper-all.sh
@@ -0,0 +1,78 @@
+cd ~/klipper
+
+echo -e -n "\e[0;31mStopping Klipper Service. "
+echo -e -n '\e[0;0m'
+
+sudo systemctl stop klipper
+sudo systemctl status klipper
+
+################################################################################
+
+git pull
+
+################################################################################
+
+# Flash main MCU - BTT Manta M8P
+make clean KCONFIG_CONFIG=klipper-btt-manta-m8p-v1.1.config
+make menuconfig KCONFIG_CONFIG=klipper-btt-manta-m8p-v1.1.config
+make -j4 KCONFIG_CONFIG=klipper-btt-manta-m8p-v1.1.config
+
+echo -e -n "\e[0;33mBTT Manta M8P MCU firmware built, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+make flash FLASH_DEVICE=/dev/ttyACM0 KCONFIG_CONFIG=klipper-btt-manta-m8p-v1.1.config
+#python3 ~/klipper/lib/canboot/flash_can.py -d /dev/serial/by-id/usb-Klipper_stm32g0b1xx_4C00130013504B4633373520-if00 -f ~/klipper/out/klipper.bin
+#python3 ~/klipper/lib/canboot/flash_can.py -d /dev/serial/by-id/usb-CanBoot_stm32g0b1xx_4C00130013504B4633373520-if00 -f ~/klipper/out/klipper.bin
+echo -e -n "\e[0;33mBTT Manta M8P MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+################################################################################
+#
+## Flash secondary MCU - BTT EBB36 v1.2
+#make clean KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+#make menuconfig KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+#make -j 4 KCONFIG_CONFIG=klipper-btt-ebb36-v1.2.config
+#
+#echo -e -n "\e[0;33mBTT EBB36 v1.2 MCU firmware built, please check above for any errors. "
+#echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+#echo -e -n '\e[0;0m'
+#read
+#
+#python3 ~/klipper/lib/canboot/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 0a754ef78da4
+#
+#echo -e -n "\e[0;33mBTT EBB36 v1.2 MCU firmware flashed, please check above for any errors. "
+#echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+#echo -e -n '\e[0;0m'
+#read
+#
+################################################################################
+
+# Flash Host MCU - Raspberry Pi
+make clean KCONFIG_CONFIG=klipper-raspberry-pi.config
+make menuconfig KCONFIG_CONFIG=klipper-raspberry-pi.config
+make -j 4 KCONFIG_CONFIG=klipper-raspberry-pi.config
+
+echo -e -n "\e[0;33mRaspberry Pi MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+make flash KCONFIG_CONFIG=klipper-raspberry-pi.config
+
+echo -e -n "\e[0;33mRaspberry Pi MCU firmware flashed, please check above for any errors. "
+echo -e -n "\e[0;33mPress [Enter] to continue flashing, or [Ctrl+C] to abort"
+echo -e -n '\e[0;0m'
+read
+
+################################################################################
+
+echo -e -n "\e[0;32mStarting Klipper Service."
+echo -e -n '\e[0;0m'
+sudo systemctl start klipper
+sudo systemctl status klipper
+
+exit 0
